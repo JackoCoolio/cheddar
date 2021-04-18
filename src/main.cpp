726e77f3b81e7e39f888ec50ad0b7217bc128279
Jackson Wambolt <jtwambolt@gmail.com>
2021-04-18 01:48:31 -0500

    Replace main() with WIP message

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,7 @@
 using namespace Cheddar;
 
 int main() {
-    // quiet move
-    Move move(coords_to_int(0, 0), coords_to_int(7, 7), 0);
-
-    std::cout << move.get_to() << std::endl;
-    std::cout << move.get_from() << std::endl;
-    std::cout << move.is_capture() << std::endl;
+    std::cout << "Cheddar's command-line interface is a work in progress." << std::endl;
 
     return 0;
 }
