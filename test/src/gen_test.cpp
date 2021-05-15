f5b44005e39fa23ebb237b5bb3bf525812da225f
Jackson Wambolt <jtwambolt@gmail.com>
2021-05-15 16:31:56 -0500

    Remove logging in gen_test

--- test/src/gen_test.cpp
+++ test/src/gen_test.cpp
@@ -23,12 +23,6 @@ TEST(Generation, move_gen) {
 
         MoveList moves = generate_legal_moves(pos);
 
-        std::cout << iter->first << std::endl;
-        std::cout << "Found " << moves.count << " moves:" << std::endl;
-        for (Move &move : moves) {
-            std::cout << "\t" << move.get_from() << "\t" << move.get_to() << "\t" << index_to_alg(move.get_from()) << index_to_alg(move.get_to()) << std::endl;
-        }
-
         if (moves.count != iter->second) {
             print_move_list(moves);
             FAIL() << "Found " << moves.count << " moves instead of " << iter->second << "!" << std::endl << iter->first << std::endl
